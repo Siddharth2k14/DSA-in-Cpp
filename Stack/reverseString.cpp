db50e4594586c9871d12dfcea6f6f82aa3b06255
Siddharth Singh <115733028+Siddharth2k14@users.noreply.github.com>
2024-08-20 20:01:07 +0000

    Adding a Question on reverse a string using stack

---
+++ Stack/reverseString.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    string str = "Siddharth";
+
+    stack<char> st;
+    for(int i = 0; i < str.length(); i++){
+        char ch = str[i];
+        st.push(ch);
+    }
+
+    string ans = "";
+
+    while(!st.empty()){
+        char ch = st.top();
+        ans.push_back(ch);
+
+        st.pop();
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
